f4d1d87cc46d31e8f704d160d8b2b1cebb6a84f0
Sirkikes <enrique.navarro@ingenieros.com>
2021-09-06 15:27:57 +0200

    primer commit

---
+++ src/main/java/com/ms/tutorial/carservice/service/CarService.java
@@ -0,0 +1,35 @@
+package com.ms.tutorial.carservice.service;
+
+import com.ms.tutorial.carservice.entity.Car;
+import com.ms.tutorial.carservice.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarService {
+
+    @Autowired
+    CarRepository carRepository;
+
+    public List<Car> getAll(){
+        return carRepository.findAll();
+    }
+
+    public Car getCarById(int id){
+        return carRepository.findById(id).orElse(null);
+    }
+
+    public Car saveCar(Car car){
+        Car carNew = carRepository.save(car);
+        return carNew;
+    }
+
+    public List<Car> byUserId(int userId){
+        return carRepository.findByUserId(userId);
+    }
+
+
+
+}
